9e17e767499fbf03100f02b6f22f3cbfcccfc5e0
Sanika Chavan <sanika16chavan.com>
2025-03-01 21:58:32 +0530

    Updated MongoDB Atlas connection

--- config/db.js
+++ config/db.js
@@ -5,14 +5,11 @@ dotenv.config(); // Load environment variables
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("✅ MongoDB Connected");
     } catch (error) {
-        console.error("❌ MongoDB Connection Error:", error);
-        process.exit(1);
+        console.error("❌ MongoDB Connection Error:", error.message);
+        process.exit(1); // Exit the process with failure
     }
 };
 
